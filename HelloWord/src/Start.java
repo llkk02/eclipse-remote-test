a408bec4e7208213e9fb1c43005277c71dafcad1
llkk02 <94101255+llkk02@users.noreply.github.com>
2021-11-15 15:57:19 +0900

    Update Start.java

--- HelloWord/src/Start.java
+++ HelloWord/src/Start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("slave branch");
+		
+		System.out.println("GitHub 수정");
 
 	}
 
